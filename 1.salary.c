c6bef0e4dc8f177bbdabf2a38b8fd5aebd1dd9cf
Yugesh A <a.yugesh2@gmail.com>
2024-10-18 22:11:18 +0530

    Update 1.salary.c

--- 1.salary.c
+++ 1.salary.c
@@ -5,18 +5,22 @@ void main()
     int empid,bp;
     float hra,da,ta,it,pf,netpay;
     char name[40];
+    //taking inputs from the user
     printf("Enter employee id : ");
     scanf("%d",&empid);
     printf("Enter the name of the employee : ");
     scanf("%s",name);
     printf("Enter the basic pay : ");
     scanf("%d",&bp);
+    //calculation
     hra = 10 / 100.0 * bp;
     da = 8 / 100.0 * bp; 
     ta = 9 / 100.0 * bp;
     it = 10 / 100.0 * bp;
     pf = 12 / 100.0 * bp;
+    
     netpay = bp + hra + da + ta - (it + pf);
+   //output 
     printf("Hi %s\n",name);
     printf("Your salary is %.2f",netpay);
 
